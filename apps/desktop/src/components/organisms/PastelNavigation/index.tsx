486af6c05fa889dcbb745132fbafa1352f3830cb
PIYoung <dlsdudg15@naver.com>
2022-02-27 02:33:16 +0900

    add: Pastel 콜렉션 추가 이벤트 바인딩

--- apps/desktop/src/components/organisms/PastelNavigation/index.tsx
+++ apps/desktop/src/components/organisms/PastelNavigation/index.tsx
@@ -1,18 +1,51 @@
 import * as Styled from './styled';
 
 import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { BsPlusCircle } from 'react-icons/bs';
 import { NavigationMenu } from '../../../interfaces/pastel.interface';
 import PastelNavigationMenu from '../../molecules/PastelNavigationMenu';
 import { RootState } from '../../../reducers';
-import { useSelector } from 'react-redux';
+import { addNewPastelCollection } from '../../../reducers/pastel.reducer';
 
 export default function PastelNavigation() {
   const { menus } = useSelector((state: RootState) => state.pastel);
+  const dispatch = useDispatch();
 
   const drawMenus = useCallback((menu: NavigationMenu, index: number) => {
     return <PastelNavigationMenu key={index} title={menu.title} childrenMenu={menu.children} />;
   }, []);
 
-  return <Styled.Container className='pt-16 p-4'>{menus.map(drawMenus)}</Styled.Container>;
+  const addCollection = useCallback(
+    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      dispatch(
+        addNewPastelCollection({
+          title: 'New Collection',
+          iconKey: 4,
+          to: '/',
+          removable: true,
+        }),
+      );
+    },
+    [dispatch],
+  );
+
+  return (
+    <Styled.Container className='pt-16 p-4'>
+      {menus.map(drawMenus)}
+      <div
+        style={{ color: 'var(--color-pastel-text' }}
+        className='text-xs flex leading-3 cursor-pointer absolute bottom-4'
+        onClick={addCollection}>
+        <div className='mr-1'>
+          <BsPlusCircle />
+        </div>
+        <div>New Collection</div>
+      </div>
+    </Styled.Container>
+  );
 }
