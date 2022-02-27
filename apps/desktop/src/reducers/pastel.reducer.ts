e73500f38e28c43840885aa6b2e5560039289c03
PIYoung <dlsdudg15@naver.com>
2022-02-28 00:51:34 +0900

    add: Paletts, Color Update & Delete Reducer Setup

--- apps/desktop/src/reducers/pastel.reducer.ts
+++ apps/desktop/src/reducers/pastel.reducer.ts
@@ -10,8 +10,14 @@ const ACTIONS = <const>{
   SET_PALETTS_LAST_VISITED: 'Pastel/SET_PALETTS_LAST_VISITED',
   SET_CURRENT_VIEW_PALETTS: 'Pastel/SET_CURRENT_VIEW_PALETTS',
   SET_NAVIGATION_MENU_RENAME_INDEX: 'Pastel/SET_NAVIGATION_MENU_RENAME_INDEX',
+  SET_PALETTS_RENAME_INDEX: 'Pastel/SET_PALETTS_RENAME_INDEX',
+  SET_COLOR_RENAME_INDEX: 'Pastel/SET_COLOR_RENAME_INDEX',
   UPDATE_NAVIGATION_MENU: 'Pastel/UPDATE_NAVIGATION_MENU',
+  UPDATE_PALETTS: 'Pastel/UPDATE_PALETTS',
+  UPDATE_COLOR: 'Pastel/UPDATE_COLOR',
   DELETE_NAVIGATION_MENU: 'Pastel/DELETE_NAVIGATION_MENU',
+  DELETE_PALETTS: 'Pastel/DELETE_PALETTS',
+  DELETE_COLOR: 'Pastel/DELETE_COLOR',
   ADD_NEW_PASTEL_COLLECTION: 'Pastel/ADD_NEW_PASTEL_COLLECTION',
   ADD_NEW_PALLETS: 'Pastel/ADD_NEW_PALLETS',
   ADD_PALLETS_NEW_COLOR: 'Pastel/ADD_PALLETS_NEW_COLOR',
@@ -36,12 +42,28 @@ export const setNavigationMenuRenameIndex = createAction(
   (payload: number) => ({ payload }),
 );
 
+export const setPalettsRenameIndex = createAction(ACTIONS.SET_PALETTS_RENAME_INDEX, (payload: number) => ({ payload }));
+
+export const setColorRenameIndex = createAction(ACTIONS.SET_COLOR_RENAME_INDEX, (payload: number) => ({ payload }));
+
 export const updateNavigationMenu = createAction(
   ACTIONS.UPDATE_NAVIGATION_MENU,
   (payload: { index: number; title: string }) => ({ payload }),
 );
 
+export const updatePaletts = createAction(ACTIONS.UPDATE_PALETTS, (payload: { index: number; title: string }) => ({
+  payload,
+}));
+
+export const updateColor = createAction(ACTIONS.UPDATE_COLOR, (payload: { index: number; title: string }) => ({
+  payload,
+}));
+
 export const deleteNavigationMenu = createAction(ACTIONS.DELETE_NAVIGATION_MENU, (payload: number) => ({ payload }));
+
+export const deletePaletts = createAction(ACTIONS.DELETE_PALETTS, (payload: number) => ({ payload }));
+
+export const deleteColor = createAction(ACTIONS.DELETE_COLOR, (payload: number) => ({ payload }));
 
 export const addNewPastelCollection = createAction(ACTIONS.ADD_NEW_PASTEL_COLLECTION, (payload: NavigationMenu) => ({
   payload,
@@ -64,6 +86,8 @@ export interface PastelState {
   selectedHex: string;
   menus: NavigationMenu[];
   renameMenuIndex?: number;
+  renamePalettsIndex?: number;
+  renameColorIndex?: number;
 }
 
 const initialState: PastelState = initialPastelState;
@@ -120,6 +144,24 @@ const pastelReducer = createReducer<PastelState>(initialState, builder => {
         },
       });
     })
+    .addCase(setPalettsRenameIndex, (state, action) => {
+      const currState = current(state);
+
+      return update(currState, {
+        renamePalettsIndex: {
+          $set: action.payload,
+        },
+      });
+    })
+    .addCase(setColorRenameIndex, (state, action) => {
+      const currState = current(state);
+
+      return update(currState, {
+        renameColorIndex: {
+          $set: action.payload,
+        },
+      });
+    })
     .addCase(updateNavigationMenu, (state, action) => {
       const currState = current(state);
       const index = 1;
@@ -133,6 +175,32 @@ const pastelReducer = createReducer<PastelState>(initialState, builder => {
                   $set: action.payload.title,
                 },
               },
+            },
+          },
+        },
+      });
+    })
+    .addCase(updatePaletts, (state, action) => {
+      const currState = current(state);
+
+      return update(currState, {
+        paletts: {
+          [action.payload.index]: {
+            title: {
+              $set: action.payload.title,
+            },
+          },
+        },
+      });
+    })
+    .addCase(updateColor, (state, action) => {
+      const currState = current(state);
+
+      return update(currState, {
+        colors: {
+          [action.payload.index]: {
+            title: {
+              $set: action.payload.title,
             },
           },
         },
@@ -155,6 +223,24 @@ const pastelReducer = createReducer<PastelState>(initialState, builder => {
               $splice: [[action.payload, 1]],
             },
           },
+        },
+      });
+    })
+    .addCase(deletePaletts, (state, action) => {
+      const currState = current(state);
+
+      return update(currState, {
+        paletts: {
+          $splice: [[action.payload, 1]],
+        },
+      });
+    })
+    .addCase(deleteColor, (state, action) => {
+      const currState = current(state);
+
+      return update(currState, {
+        colors: {
+          $splice: [[action.payload, 1]],
         },
       });
     })
