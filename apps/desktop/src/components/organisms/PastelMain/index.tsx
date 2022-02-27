cf74b76904312a75756b50fcbb0aa59ea990e847
PIYoung <dlsdudg15@naver.com>
2022-02-27 09:13:49 +0900

    fix: Pastel Main Padding

--- apps/desktop/src/components/organisms/PastelMain/index.tsx
+++ apps/desktop/src/components/organisms/PastelMain/index.tsx
@@ -3,5 +3,5 @@ import * as Styled from './styled';
 import React from 'react';
 
 export default function PastelMain() {
-  return <Styled.Container>PastelMain</Styled.Container>;
+  return <Styled.Container className='p-4'>PastelMain</Styled.Container>;
 }
