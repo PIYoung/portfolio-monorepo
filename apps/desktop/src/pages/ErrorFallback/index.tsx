a445017b679429c2502583abc776a435d2ae5feb
PIYoung <dlsdudg15@naver.com>
2022-02-28 12:04:44 +0900

    add: ErrorFallback 스타일링

--- apps/desktop/src/pages/ErrorFallback/index.tsx
+++ apps/desktop/src/pages/ErrorFallback/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import { BsHouseDoor } from 'react-icons/bs';
+import { PATHS } from '../../constants';
 
 interface Props {
   error: Error;
@@ -6,11 +9,29 @@ interface Props {
 }
 
 export default function ErrorFallback({ error, resetErrorBoundary }: Props) {
+  const goHome = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    window.location.href = PATHS.MAIN;
+  }, []);
+
   return (
-    <div role='alert'>
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
+    <div
+      className='flex justify-center items-center text-center w-full'
+      style={{ height: '100vh', backgroundColor: 'var(--color-background)', color: 'var(--color-text)' }}>
+      <div>
+        <h1 className='text-8xl mb-16'>Error Page</h1>
+        <p className='text-lg'>에러가 발생했습니다.</p>
+        <p className='text-lg'>잠시 후 다시 시도해주세요.</p>
+        <div className='mt-16 cursor-pointer' onClick={goHome}>
+          <div className='flex justify-center items-center p-2 text-lg border rounded'>
+            <div className='mr-2'>
+              <BsHouseDoor size={20} />
+            </div>
+            <div>돌아가기</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
