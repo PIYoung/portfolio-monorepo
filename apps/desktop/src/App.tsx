65f6ec5aeae568a78aba8eadc184312a9f398d23
PIYoung <dlsdudg15@naver.com>
2022-02-28 12:03:56 +0900

    delete: loadable component node mondule
    깜빡임 제거

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -2,30 +2,24 @@ import './App.css';
 import 'intro.js/introjs.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import React, { useCallback } from 'react';
 
 import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './pages/ErrorFallback';
 import { GlobalStyles } from './components/templates/GlobalStyles';
+import Main from './pages/Main';
 import { PATHS } from './constants';
+import PalettsDetail from './pages/PalettsDetail';
+import Pastel from './pages/Pastel';
+import React from 'react';
 import { RootState } from './reducers';
-import loadable from '@loadable/component';
+import ThreeJS from './pages/ThreeJS';
 import { useSelector } from 'react-redux';
-
-const Main = loadable(() => import('./pages/Main'));
-const ThreeJS = loadable(() => import('./pages/ThreeJS'));
-const Pastel = loadable(() => import('./pages/Pastel'));
-const PalettsDetail = loadable(() => import('./pages/PalettsDetail'));
-const ErrorFallback = loadable(() => import('./pages/ErrorFallback'));
 
 function App() {
   const { configurations } = useSelector((state: RootState) => state.user);
 
-  const handleError = useCallback((error: Error, info: { componentStack: string }) => {
-    console.log('hi');
-  }, []);
-
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback} onError={handleError}>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
       <GlobalStyles colorTheme={configurations.theme} />
       <BrowserRouter>
         <Routes>
@@ -33,14 +27,7 @@ function App() {
           <Route path={PATHS.THREE_JS} element={<ThreeJS />} />
           <Route path={PATHS.PASTEL} element={<Pastel />} />
           <Route path={PATHS.PALETTS_DETAIL} element={<PalettsDetail />} />
-          <Route
-            path='*'
-            element={
-              <main style={{ padding: '1rem' }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
+          <Route path='*' element={<ErrorFallback error={new Error('404')} resetErrorBoundary={() => {}} />} />
         </Routes>
       </BrowserRouter>
     </ErrorBoundary>
