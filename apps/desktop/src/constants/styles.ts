066324536eb802d56cb34b04c5196afdfae6f95a
PIYoung <dlsdudg15@naver.com>
2022-03-03 15:04:31 +0900

    fix: dark mode box-shadow rgb(255 -> 155) & all mode box-shadow alpha(0.35 -> 0.15)

--- apps/desktop/src/constants/styles.ts
+++ apps/desktop/src/constants/styles.ts
@@ -12,8 +12,8 @@ export const COLORS = <const>{
     pastelText: '#eaeaea',
     pastelTextSecondary: '#646968',
     boxShadow:
-      'rgba(255, 255, 255, 0.07) 0px 1px 2px, rgba(255, 255, 255, 0.07) 0px 2px 4px, rgba(255, 255, 255, 0.07) 0px 4px 8px, rgba(255, 255, 255, 0.07) 0px 8px 16px, rgba(255, 255, 255, 0.07) 0px 16px 32px, rgba(255, 255, 255, 0.07) 0px 32px 64px',
-    boxShadowSecondary: 'rgba(255, 255, 255, 0.35) 0px 2px 8px',
+      'rgba(155, 155, 155, 0.07) 0px 1px 2px, rgba(155, 155, 155, 0.07) 0px 2px 4px, rgba(155, 155, 155, 0.07) 0px 4px 8px, rgba(155, 155, 155, 0.07) 0px 8px 16px, rgba(155, 155, 155, 0.07) 0px 16px 32px, rgba(155, 155, 155, 0.07) 0px 32px 64px',
+    boxShadowSecondary: 'rgba(155, 155, 155, 0.15) 0px 2px 8px',
   },
   light: {
     text: '#000000',
@@ -29,7 +29,7 @@ export const COLORS = <const>{
     pastelTextSecondary: '#2e2e2e',
     boxShadow:
       'rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px',
-    boxShadowSecondary: 'rgba(0, 0, 0, 0.35) 0px 2px 8px',
+    boxShadowSecondary: 'rgba(0, 0, 0, 0.15) 0px 2px 8px',
   },
 };
 
